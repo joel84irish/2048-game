090c8ac917f45b8000dda6fc1dac7455c7e9d8fa
joel84irish <125594109+joel84irish@users.noreply.github.com>
2025-02-26 15:34:27 +0000

    Update dns.tf

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -1,22 +1,22 @@
-data "aws_route53_zone" "lab" {
-  name = "lab.moabukar.co.uk"
+data "aws_route53_zone" "2048" {
+  name = "2048.joelirish-game-project.app"
 }
 
-resource "aws_route53_record" "tm" {
+resource "aws_route53_record" "2048" {
   zone_id = data.aws_route53_zone.lab.zone_id
   name    = "tm.${data.aws_route53_zone.lab.name}"
   type    = "A"
 
   alias {
-    name                   = aws_lb.tm_alb.dns_name
-    zone_id                = aws_lb.tm_alb.zone_id
+    name                   = aws_lb.2048_alb.dns_name
+    zone_id                = aws_lb.2048_alb.zone_id
     evaluate_target_health = true
   }
 }
 
 ## HTTPS/DNS
-resource "aws_acm_certificate" "tm_cert" {
-  domain_name       = "tm.lab.moabukar.co.uk"
+resource "aws_acm_certificate" "2048_cert" {
+  domain_name       = "2048.joelirish-game-project.app
   validation_method = "DNS"
 
   lifecycle {
@@ -24,7 +24,7 @@ resource "aws_acm_certificate" "tm_cert" {
   }
 }
 
-resource "aws_route53_record" "tm_cert_validation" {
+resource "aws_route53_record" "2048_cert_validation" {
   for_each = {
     for dvo in aws_acm_certificate.tm_cert.domain_validation_options : dvo.domain_name => {
       name   = dvo.resource_record_name
@@ -42,6 +42,6 @@ resource "aws_route53_record" "tm_cert_validation" {
 }
 
 resource "aws_acm_certificate_validation" "tm_cert" {
-  certificate_arn         = aws_acm_certificate.tm_cert.arn
-  validation_record_fqdns = [for record in aws_route53_record.tm_cert_validation : record.fqdn]
+  certificate_arn         = aws_acm_certificate.2048_cert.arn
+  validation_record_fqdns = [for record in aws_route53_record.2048_cert_validation : record.fqdn]
 }
