b99ade4229d90804575e693edf423e0f90859596
joel84irish <125594109+joel84irish@users.noreply.github.com>
2025-02-26 15:38:15 +0000

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,8 +2,8 @@ module "ecs_app" {
   source                       = "./modules/ecs"
   ec2_task_execution_role_name = "EcsTaskExecutionRoleName"
   ecs_auto_scale_role_name     = "EcsAutoScaleRoleName"
-  app_image                    = "766261352911.dkr.ecr.us-west-2.amazonaws.com/app"
-  app_port                     = 3000
+  app_image                    = "980921749029.dkr.ecr.us-west-2.amazonaws.com/2048"
+  app_port                     = 80
   app_count                    = 1
   health_check_path            = "/"
   fargate_cpu                  = "1024"
