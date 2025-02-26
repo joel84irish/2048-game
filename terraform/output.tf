b1b074d681c364ff92e68ad9c093512591dc28f9
joel84irish <125594109+joel84irish@users.noreply.github.com>
2025-02-26 15:39:34 +0000

    Update output.tf

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,14 +1,14 @@
 output "alb_url" {
   description = "The URL of the Application Load Balancer"
-  value       = aws_lb.tm_alb.dns_name
+  value       = aws_lb.2048_alb.dns_name
 }
 
 output "application_url" {
   description = "The URL of the application"
-  value       = "http://${aws_route53_record.tm.name}"
+  value       = "http://${aws_route53_record.2048.name}"
 }
 
 output "https_application_url" {
   description = "The URL of the application"
-  value       = "https://${aws_route53_record.tm.name}"
+  value       = "https://${aws_route53_record.2048.name}"
 }
