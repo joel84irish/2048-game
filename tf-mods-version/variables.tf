54d4cccba94b979222cff14c0e488d8e170cf3b8
joel84irish <125594109+joel84irish@users.noreply.github.com>
2025-02-26 16:17:50 +0000

    Update variables.tf

--- tf-mods-version/variables.tf
+++ tf-mods-version/variables.tf
@@ -5,7 +5,7 @@ variable "project_name" {
 }
 
 variable "domain_name" {
-  default     = "joelirish.app"
+  default     = "joelirish-game-project.app"
   description = "The domain name to use for the project"
   type        = string
 }
