1d3152ad2fc46bc4089b4153ecfd5eb1e798d635
joel84irish <125594109+joel84irish@users.noreply.github.com>
2025-02-26 16:23:23 +0000

    Update main.tf

--- tf-mods-version/modules/networking/main.tf
+++ tf-mods-version/modules/networking/main.tf
@@ -56,13 +56,6 @@ resource "aws_security_group" "ecs_tasks" {
   vpc_id      = aws_vpc.main.id
 
   ingress {
-    from_port   = 3000
-    to_port     = 3000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
